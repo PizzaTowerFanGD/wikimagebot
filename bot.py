ac7f0b7af98502347bf75d92e5c6a8e850cbe1fa
PizzaTowerFanGD <121449764+PizzaTowerFanGD@users.noreply.github.com>
2025-05-11 08:41:08 -0400

    Update bot.py

--- bot.py
+++ bot.py
@@ -4,46 +4,41 @@ import os
 
 MASTODON_TOKEN = os.getenv('MASTODON_TOKEN')
 
+# Fetch a random image instead of an article
 res = requests.get("https://en.wikipedia.org/w/api.php", params={
     "action": "query",
     "generator": "random",
-    "grnnamespace": 0,
-    "prop": "images",
+    "grnnamespace": 6,  # Namespace 6 corresponds to files (including images)
+    "prop": "imageinfo",
+    "iiprop": "url",
     "format": "json"
 })
 res.raise_for_status()
 data = res.json()
 
+# Get the random image and its URL
 page = next(iter(data['query']['pages'].values()))
 title = page['title']
-images = page.get('images', [])
-image_title = next((img['title'] for img in images if img['title'].lower().endswith(('.jpg', '.jpeg', '.png'))), None)
-
-if not image_title:
-    print("no suitable image found")
+imageinfo = page.get('imageinfo', [])
+if not imageinfo:
+    print("No image info found")
     exit(0)
 
-image_info = requests.get("https://en.wikipedia.org/w/api.php", params={
-    "action": "query",
-    "titles": image_title,
-    "prop": "imageinfo",
-    "iiprop": "url",
-    "format": "json"
-}).json()
+image_url = imageinfo[0]['url']
 
-image_url = next(iter(image_info['query']['pages'].values()))['imageinfo'][0]['url']
-
+# Download the image
 img_data = requests.get(image_url).content
 with open("temp.jpg", "wb") as f:
     f.write(img_data)
 
+# Post to Mastodon
 mastodon = Mastodon(
     access_token=MASTODON_TOKEN,
     api_base_url='https://mastodon.social'
 )
 
 media = mastodon.media_post("temp.jpg", sensitive=True)
-status = f'"{title}"\n{image_url} (BOT POST, MAY CONTAIN BAD CONTENT)'
+status = f'Random Wikipedia Image: "{title}"\n{image_url} (BOT POST, MAY CONTAIN BAD CONTENT)'
 mastodon.status_post(status=status, media_ids=[media])
 
 print("posted:", status)
